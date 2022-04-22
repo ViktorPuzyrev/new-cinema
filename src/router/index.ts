19faa66576e6c08edaa2c8e9e5c0c43078ae0de8
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-04-22 13:01:34 +0400

    Add page title meta

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,24 +6,31 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "home",
     component: HomeView,
+    meta: { title: "Главная страница - New Cinema" },
   },
   {
     path: "/about",
     name: "about",
     component: () =>
       import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
+    meta: { title: "О проекте - New Cinema" },
   },
   {
     path: "/cart",
     name: "cart",
     component: () =>
       import(/* webpackChunkName: "about" */ "@/views/CartView.vue"),
+    meta: { title: "Мои билеты - New Cinema" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to) => {
+  document.title = (to.meta.title as string) || "Some Default Title";
 });
 
 export default router;
