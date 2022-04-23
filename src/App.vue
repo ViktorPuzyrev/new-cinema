f71f0d944f14721701b77e50af448f87a2515c35
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-04-23 21:22:06 +0400

    Add display breakpoint variable

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,12 @@
 </template>
 
 <script setup lang="ts">
+import { useDisplay } from "vuetify";
+import store from "./store";
 import TheHeader from "@/components/TheHeader.vue";
+
+(function setBreakpoint() {
+  const { name } = useDisplay();
+  store.commit("updateBreakpoint", name.value);
+})();
 </script>
