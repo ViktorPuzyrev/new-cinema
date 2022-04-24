195d586745d44b6bc4d51316085c283d17e61f11
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-04-24 20:44:41 +0400

    Add slider component

---
+++ src/components/TheSlider.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="loading">
+    <span>Loading...</span>
+  </div>
+  <div v-else>
+    <v-carousel cycle height="100%" hide-delimiters show-arrows="hover">
+      <v-carousel-item v-for="movie in UpcomingMovies" :key="movie.id" eager>
+        <v-img :src="movie.backdrop_path" contain eager></v-img>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useStore } from "vuex";
+import { Movie } from "@/store/types";
+
+const store = useStore();
+const loading = computed((): boolean => store.state.loading);
+const UpcomingMovies = computed(
+  (): Movie[] => store.getters.upcomingMoviesList
+);
+</script>
+
+<style scoped></style>
