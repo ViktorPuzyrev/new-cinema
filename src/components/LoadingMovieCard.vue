4a5777aa4683e1975a2c9861c16cfa9ccabb66c5
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-04-30 20:42:10 +0400

    Add loading component for MovieCard

---
+++ src/components/LoadingMovieCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="pa-3 mb-3">
+    <v-row no-gutter class="d-flex" cols="12">
+      <v-col>
+        <v-card width="100%" height="260" class="d-flex">
+          <v-progress-circular
+            class="ma-auto"
+            indeterminate
+            color="primary"
+            :size="70"
+            :width="7"
+          >
+          </v-progress-circular>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
