8259c4ebd767f9c1b254813df598db92311216b5
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-05-03 14:48:27 +0400

    Code refactoring

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,49 +2,47 @@
   <TheSlider />
   <v-card class="mt-4 pa-3" color="primary">
     <span class="text-h5">Уже на экранах</span>
-    <Suspense>
+    <Suspense v-for="movie of movieList" :key="movie.kinopoiskId">
       <template #default>
-        <MovieCard
-          v-for="movie of moviesPagination[0]"
-          :key="movie.kinopoiskId"
-          :kinopoiskId="movie.kinopoiskId"
-        />
+        <MovieCard :kinopoiskId="movie.kinopoiskId" />
       </template>
       <template #fallback>
         <div>
-          <LoadingMovieCard v-for="n of 10" :key="n" />
+          <LoadingMovieCard />
         </div>
       </template>
     </Suspense>
     <div class="d-flex">
-      <v-btn class="text-black ma-auto">Показать еще</v-btn>
+      <v-btn class="text-black ma-auto" @click="addToMovieList"
+        >Показать еще</v-btn
+      >
     </div>
   </v-card>
 </template>
 
 <script setup lang="ts">
+import { computed, reactive, watch } from "vue";
 import { useStore } from "vuex";
 import TheSlider from "@/components/TheSlider.vue";
 import MovieCard from "@/components/MovieCard.vue";
 import LoadingMovieCard from "@/components/LoadingMovieCard.vue";
-import { computed } from "vue";
 import { Movie } from "@/store/types";
 
-const store = useStore();
+const movieList: Movie[] = reactive([]);
 
+const store = useStore();
 const NowPlayingMovies = computed(
   (): Movie[] => store.getters.NowPlayingMoviesList
 );
-
-function chunk(arr: Movie[], size: number) {
-  const result = [];
-  for (let i = 0; i < Math.ceil(arr.length / size); i++) {
-    result.push(arr.slice(i * size, i * size + size));
-  }
-  return result;
+watch(
+  () => NowPlayingMovies.value,
+  () => movieList.push(...NowPlayingMovies.value.slice(0, 5))
+);
+function addToMovieList() {
+  movieList.push(
+    ...NowPlayingMovies.value.slice(movieList.length, movieList.length + 5)
+  );
 }
-
-const moviesPagination = computed(() => chunk(NowPlayingMovies.value, 10));
 
 const initData = () => {
   store.dispatch("initUpcomingMovies");
