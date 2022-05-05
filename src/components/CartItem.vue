0991d82e57e2821f57be8f0561c76a5b90c0eb21
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-05-05 21:12:44 +0400

    Add CartItem component

---
+++ src/components/CartItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card class="mb-4"> asd </v-card>
+</template>
+
+<script setup lang="ts">
+import { ref, PropType } from "vue";
+import { CartItem } from "@/store/types";
+
+const props = defineProps({
+  item: { type: Object as PropType<CartItem>, required: true },
+});
+</script>
+
+<style scoped></style>
