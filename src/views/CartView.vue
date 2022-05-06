4e606cf57e11dfea7cc1f8f6c26665e6ba0fd0f1
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-05-06 20:48:33 +0400

    Add cart items list

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,7 +1,33 @@
 <template>
-  <p>Мои билеты</p>
+  <v-card class="pa-3" color="blue-grey">
+    <span class="text-h5 text-white">Мои билеты</span>
+    <div v-if="emptyCart">
+      <v-card><v-card-title>Козина пуста</v-card-title></v-card>
+    </div>
+    <div v-else>
+      <CartItem
+        v-for="(item, index) of cartItems"
+        :item="item"
+        :index="index"
+        :key="item.movieId"
+      />
+    </div>
+    <div class="d-flex justify-end">
+      <v-btn v-if="!emptyCart">Оплатить {{ cartItemsCost }}</v-btn>
+    </div>
+  </v-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { useStore } from "vuex";
+import CartItem from "@/components/CartItem.vue";
+import * as TYPE from "@/store/types";
 
-<style></style>
+const store = useStore();
+const emptyCart = computed(() => !store.getters.cartItemsCount);
+const cartItems = computed<TYPE.CartItem[]>(() => store.state.cart);
+const cartItemsCost = computed<number>(() => store.getters.cartTotalCoast);
+</script>
+
+<style scoped></style>
