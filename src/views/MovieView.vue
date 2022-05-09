313311d92e833b257a4b673219406891d0f2536b
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-05-09 19:28:50 +0400

    Refactor code

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,9 +1,24 @@
 <template>
-  <Suspense> <MovieInfo /></Suspense>
+  <Suspense>
+    <v-row>
+      <v-col cols="12" xs="12" sm="4" md="4" lg="3" xl="">
+        <MoviePoster
+      /></v-col>
+      <v-col cols="12" xs="12" sm="8" md="8" lg="" xl=""
+        ><MovieInfo class="mb-4" />
+        <MovieYouTubeTrailer />
+      </v-col>
+      <v-col cols="12" xs="12" sm="" md="6" lg="3" xl=""
+        ><MovieCredits
+      /></v-col> </v-row
+  ></Suspense>
 </template>
 
 <script setup lang="ts">
 import MovieInfo from "@/components/MovieInfo.vue";
+import MoviePoster from "@/components/MoviePoster.vue";
+import MovieYouTubeTrailer from "@/components/MovieYouTubeTrailer.vue";
+import MovieCredits from "@/components/MovieCredits.vue";
 </script>
 
 <style scoped></style>
