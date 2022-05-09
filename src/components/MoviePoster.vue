b6c726b2d23aacd966c91c645a8ff1176067cbbb
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-05-09 19:19:44 +0400

    Add MoviePoster component

---
+++ src/components/MoviePoster.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card><v-img :src="posterUrl" cover></v-img></v-card>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import useAPI from "@/services/API";
+import { MovieDetails } from "@/store/types";
+
+const route = useRoute();
+const { posterUrl }: MovieDetails = await useAPI(`movie/${route.params.id}`);
+</script>
+
+<style scoped></style>
