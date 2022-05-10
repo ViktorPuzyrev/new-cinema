68ca64ab2f79267d4e1ffc2ae327329f9617d678
Viktor Puzyrev <v.puzyrev.pktm@gmail.com>
2022-05-10 21:15:43 +0400

    Add ApiAlert component

---
+++ src/components/ApiAlert.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-alert v-if="store.state.error" type="error" class="mb-4"
+    >Ошибка API. {{ store.state.error }}</v-alert
+  >
+</template>
+
+<script setup lang="ts">
+import { useStore } from "vuex";
+
+const store = useStore();
+</script>
+
+<style scoped></style>
